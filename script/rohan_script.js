fe3c01154b7dfb59a5b339e775d01f426e02908a
RohanBera <berarohan88@gmail.com>
2020-05-29 11:31:48 +0530

    tooptip placement adjusted

--- script/rohan_script.js
+++ script/rohan_script.js
@@ -10,62 +10,63 @@ var imported = document.createElement('script');
 imported.src = '../script/table.js';
 document.head.appendChild(imported);
 
-// displays states when clicked
+// heading variables
+var main_heading = document.querySelector('#main_heading');
+var side_heading = document.querySelector('#side_heading');
 
+
+// displays states when clicked
 function view_state(evt, state) {
     document.getElementById("India").style.display = "none";
 
-    var disp_state = state.id + " districts";
+    var disp_state = state.id+ " districts";
     document.getElementById(disp_state).style.display = "block";
 
     var backbtn = document.getElementById('backbtn');
     backbtn.style.display = "block";
     backbtn.name = disp_state; // temporarily maintaining which state is selected
+
+    main_heading.innerHTML = state.id;
 }
 
 // plot function
-
-// function plot(state, district) {
-//     console.log(state, district);
-//     // plotGraph(state, district)
-//     plot(state, district)
-// }
+function plot(state, district) {
+    console.log(state, district);
+    // plotGraph(state, district)
+    plot(state, district)
+}
 
 
 // back to India map from state
-
 function back(buttonObj) {
     document.getElementById("India").style.display = "block";
     document.getElementById(buttonObj.name).style.display = "none";
 
     buttonObj.style.display = "none";
     buttonObj.name = "";
+
+    main_heading.innerHTML = "India"
 }
 
-// tooltip code
-
+// tooltips and headings
 var tooltip = document.querySelector('#tooltip');
 
-[].forEach.call(document.querySelectorAll('path'), function (item) {
-    item.addEventListener('mouseenter', function () {
-        var sel = this, pos = sel.getBoundingClientRect()
-
+[].forEach.call(document.querySelectorAll('path'), function(item) {
+    item.addEventListener('mouseenter', function(event) {
         tooltip.innerHTML = this.id;
-
         tooltip.style.display = 'block';
-        tooltip.style.top = pos.top + 100 + 'px';
-        tooltip.style.left = pos.left + 'px';
 
+        var x = event.pageX;    
+        var y = event.pageY;  
 
-        // var x = event.screenX;    
-        // var y = event.screenY;  
+        tooltip.style.top = y + 'px';
+        tooltip.style.left = x + 'px';
 
-        // tooltip.style.top = y + 'px';
-        // tooltip.style.left = x + 'px';
-
+        side_heading.innerHTML = this.id;
     });
-
-    item.addEventListener('mouseleave', function () {
+  
+    item.addEventListener('mouseleave', function(){
         tooltip.style.display = 'none';
+        side_heading.innerHTML = '&nbsp;';
     });
 });
