641cc47d9923a349b4b009f8e1760e27e2cd6346
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-06-03 17:31:51 +0530

    Calculation of page hits

--- script/calculate_page_hits.js
+++ script/calculate_page_hits.js
@@ -1,71 +1,46 @@
-const express = require('express')
-const fs = require('fs')
-const bodyParser = require('body-parser');
+HITS_API = "http://40.76.33.143:3000"
 
-var app = express()
+var getCurrentDate = () => {
+    let date_ob = new Date();
 
-HITS_FILE = './hits.json'
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
 
-var writeJSONtoFile = (jsonObject, filename) => {
-    var jsonContent = JSON.stringify(jsonObject);
-
-    fs.writeFile(filename, jsonContent, 'utf8', function (err) {
-        if (err) {
-            console.log("An error occured while writing JSON Object to File.");
-            return console.log(err);
-        }
-
-        console.log("JSON file " + filename + " has been saved.");
-    });
+    return year + "-" + month + "-" + date
 }
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var getCurrentTime = () => {
+    let date_ob = new Date();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+    let hours = date_ob.getHours()
+    let minutes = date_ob.getMinutes()
+    let seconds = date_ob.getSeconds();
 
-app.get('/', function (req, res) {
-    fs.readFile(__dirname + '/' + HITS_FILE, "utf8", function (err, data) {
-        console.log(data)
-        res.writeHead(200, { 'Content-Type': 'json' })
-        res.end(data)
-    })
-})
+    return hours + ":" + minutes + ":" + seconds
+}
 
-app.post('/', function (req, res) {
-    date = req.body.date
-    time = req.body.time
+var updateHits = async () => {
+    currentDate = getCurrentDate()
+    currentTime = getCurrentTime()
 
-    if (!date || !time) {
-        console.log("Invalid Post request!")
-        res.writeHead(304, "No valid date and time have been provided!")
-    } else {
-        fs.readFile(__dirname + '/' + HITS_FILE, "utf8", function (err, data) {
-            jsonObject = JSON.parse(data)
-            if (date in jsonObject) {
-                console.log(date + " is in ", jsonObject)
-                jsonObject[date]["timeStamps"].push(time)
-                jsonObject[date]["count"] += 1
-                writeJSONtoFile(jsonObject, HITS_FILE)
-            } else {
-                console.log(date + " is not in ", jsonObject)
-                jsonObject[date] = { "timeStamps": [], "count": 0 }
-                jsonObject[date]["timeStamps"].push(time)
-                jsonObject[date]["count"] += 1
-                writeJSONtoFile(jsonObject, HITS_FILE)
-            }
-        })
-        res.writeHead(201, "Added new time stamp")
+    data = {
+        "date": currentDate,
+        "time": currentTime
     }
-    res.end()
-})
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://<%s>:%s", host, port)
-})
+    response = await fetch(HITS_API, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data),
+    })
+}
+
+window.addEventListener('load', updateHits)
